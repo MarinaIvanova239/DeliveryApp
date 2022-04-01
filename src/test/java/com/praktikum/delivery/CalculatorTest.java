063902684588e54e577b7997033972cd9b4cfb11
Marina.Ivanova <Ijyf23011996!>
2022-04-01 23:16:10 +0300

    add negative test cases

--- src/test/java/com/praktikum/delivery/CalculatorTest.java
+++ src/test/java/com/praktikum/delivery/CalculatorTest.java
@@ -20,6 +20,7 @@ public class CalculatorTest {
     @DataProvider(name="deliveryParameters")
     public Object[][] deliveryParameters() {
         return new Object[][] {
+                // distances
                 { 1, Dimension.small, true, Workload.standard, 450 },
                 { 2, Dimension.small, true, Workload.standard, 450 },
                 { 3, Dimension.small, true, Workload.standard, 500 },
@@ -29,23 +30,25 @@ public class CalculatorTest {
                 { 29, Dimension.small, true, Workload.standard, 600 },
                 { 30, Dimension.small, true, Workload.standard, 600 },
                 { 31, Dimension.small, false, Workload.standard, 400 },
-
+                { 40, Dimension.small, false, Workload.standard, 400 },
+                // dimensions
                 { 2, Dimension.large, true, Workload.standard, 550 },
                 { 30, Dimension.large, true, Workload.standard, 700 },
-
+                // workloads
                 { 29, Dimension.small, true, Workload.increased, 720 },
                 { 29, Dimension.small, true, Workload.high, 840 },
                 { 29, Dimension.small, true, Workload.very_high, 960 },
                 { 31, Dimension.small, false, Workload.increased, 480 },
                 { 31, Dimension.small, false, Workload.high, 560 },
                 { 31, Dimension.small, false, Workload.very_high, 640 },
-
+                // fragility
+                { 29, Dimension.small, false, Workload.very_high, 480 },
+                // min available price
                 { 9, Dimension.small, false, Workload.standard, 400 },
                 { 9, Dimension.small, false, Workload.high, 400 },
                 { 29, Dimension.small, false, Workload.standard, 400 },
                 { 29, Dimension.small, false, Workload.high, 420 }
         };
-
     }
 
     @Test(dataProvider = "deliveryParameters")
@@ -55,15 +58,25 @@ public class CalculatorTest {
         assertThat("Expected price =" + expectedPrice + ", but actual = " + actualPrice, expectedPrice == actualPrice);
     }
 
-
-    @Test(expectedExceptions = IncorrectDeliveryException.class)
-    public void fragileThingsCannotBeDeliveredForLongDistances() throws Exception {
-        calculator.countDelivery(31, Dimension.large, true, Workload.high);
+    @DataProvider(name="incorrectDeliveryParameters")
+    public Object[][] incorrectDeliveryParameters() {
+        return new Object[][] {
+                // fragile things for long distances
+                { 31, Dimension.large, true, Workload.high },
+                { 31, Dimension.small, true, Workload.standard },
+                { 39, Dimension.large, true, Workload.high },
+                // incorrect distances
+                { 0, Dimension.small, true, Workload.standard },
+                { -1, Dimension.small, true, Workload.standard }
+        };
     }
 
-    @Test(expectedExceptions = IncorrectDeliveryException.class)
-    public void fragileThingsCanBeDeliveredForShortDistances() throws Exception {
-        calculator.countDelivery(30, Dimension.large, true, Workload.high);
+
+    @Test(expectedExceptions = IncorrectDeliveryException.class, dataProvider = "incorrectDeliveryParameters")
+    public void exceptionShouldBeThrownWithIncorrectParameters(int distance, Dimension dimension, boolean isFragile,
+                                                               Workload workload) throws Exception {
+        calculator.countDelivery(distance, dimension, isFragile, workload);
     }
+
 
 }
